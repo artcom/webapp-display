6ed0e1d3ca7076cf14b05423fcd5655e6cf5fe16
Stefan Dieterle <stefand@artcom.de>
2021-11-25 09:49:10 +0100

    Build mac and linux apps

--- scripts/package.sh
+++ scripts/package.sh
@@ -2,12 +2,19 @@
 
 set -eo pipefail
 
-BUILD_DIR="build/win-unpacked"
-ARTIFACTS_DIR="artifacts"
-
-echo "Packaging"
+declare -a platforms=("win" "mac" "linux")
+declare -a build_dirs=("build/win-unpacked" "build/mac/webapp-display.app" "build/linux-unpacked")
+PROJECT_DIR=$PWD
+ARTIFACTS_DIR="${PROJECT_DIR}/artifacts"
 mkdir "${ARTIFACTS_DIR}"
-cd "${BUILD_DIR}"
-echo {\"branch\": \"$TRAVIS_BRANCH\", \"commit\": \"$TRAVIS_COMMIT\", \"build\": $TRAVIS_BUILD_NUMBER} > build.json
-zip -r -9 "../../${ARTIFACTS_DIR}/webapp-display-${TRAVIS_BRANCH}.zip" *
-tar czvf "../../${ARTIFACTS_DIR}/webapp-display-${TRAVIS_BRANCH}.tar.gz" *
+
+
+for (( i=0; i<${#platforms[@]}; i++ ));
+do
+  echo "Packaging for ${platforms[$i]} platform"
+  cd "${build_dirs[$i]}"
+  echo {\"branch\": \"$TRAVIS_BRANCH\", \"commit\": \"$TRAVIS_COMMIT\", \"build\": $TRAVIS_BUILD_NUMBER} > build.json
+  zip -r -9 "${ARTIFACTS_DIR}/webapp-display-${platforms[$i]}-${TRAVIS_BRANCH}.zip" *
+  tar czvf "${ARTIFACTS_DIR}/webapp-display-${platforms[$i]}-${TRAVIS_BRANCH}.tar.gz" *
+  cd $PROJECT_DIR
+done
