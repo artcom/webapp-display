7dfab36fc043196f033bba83b2a231d8d7f42218
Mirko Geissler <mirko.geissler@artcom.de>
2022-05-17 17:07:47 +0200

    Add missing parameter

--- src/window.js
+++ src/window.js
@@ -8,7 +8,7 @@ module.exports.createWindow = (sessionId, url, bounds, displayId, logger) => {
   })
   const isfullscreen = bounds === null
   const windowBounds = { x: 0, y: 0, width: 800, height: 600, ...bounds }
-  const display = getDisplay(displayId)
+  const display = getDisplay(displayId, logger)
 
   const options = {
     fullscreen: isfullscreen,
