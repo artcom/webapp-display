9896eb9124bed3ac7a648e859005eb1d01de822d
Stefan Dieterle <stefand@artcom.de>
2023-10-30 12:27:30 +0100

    Update builder containers

--- scripts/create-artifacts.sh
+++ scripts/create-artifacts.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 set -eo pipefail
 
-docker run --rm -v $PWD:/project electronuserland/builder:16-wine scripts/build.sh
-docker run -v $PWD:/app --workdir /app -e COMMIT_TAG -e COMMIT_HASH -e CI_JOB_ID -e REPOSITORY -e OWNER debian:bullseye-slim scripts/package.sh
+docker run --rm -v $PWD:/project electronuserland/builder:18-wine scripts/build.sh
+docker run -v $PWD:/app --workdir /app -e COMMIT_TAG -e COMMIT_HASH -e CI_JOB_ID -e REPOSITORY -e OWNER debian:bookworm-slim scripts/package.sh
