a9915d05b08492064c24e1e25b5f43d7df2c3690
Philipp Niederlag <philipp.niederlag@artcom.de>
2025-12-19 13:57:10 +0100

    Log objects

--- src/preload.js
+++ src/preload.js
@@ -1,3 +1,46 @@
-const { webFrame } = require("electron")
+const { webFrame, ipcRenderer } = require("electron")
 
 webFrame.setVisualZoomLevelLimits(1, 1)
+
+const logLevels = ["log", "info", "warn", "error", "debug"]
+logLevels.forEach((level) => {
+  const original = console[level]
+
+  console[level] = (...args) => {
+    ipcRenderer.send("renderer-console", {
+      level,
+      message: args.map(serialize),
+    })
+
+    original.apply(console, args)
+  }
+})
+
+function serialize(arg) {
+  if (arg instanceof Error) {
+    return {
+      __type: "Error",
+      name: arg.name,
+      message: arg.message,
+      stack: arg.stack,
+    }
+  }
+
+  if (typeof arg === "function") {
+    return `[Function: ${arg.name || "anonymous"}]`
+  }
+
+  if (arg instanceof HTMLElement) {
+    return `[HTMLElement: ${arg.tagName}]`
+  }
+
+  if (arg === window) return "[Window]"
+  if (arg === document) return "[Document]"
+
+  try {
+    structuredClone(arg)
+    return arg
+  } catch (e) {
+    return Object.prototype.toString.call(arg)
+  }
+}
